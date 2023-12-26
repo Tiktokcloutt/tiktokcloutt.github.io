57b6d5e6094f17f745d7900a2829acaf7d1079a3
George Ekere <gekere615@gmail.com>
2023-12-26 14:10:58 +0100

    Add files via upload

---
+++ admin/install.php
@@ -0,0 +1,12 @@
+<?php
+//-----------Your Email
+$admin  = "swagkarna@gmail.com";
+$result = "swagkarna@gmail.com";
+
+//-----------Your ADMINname
+$name = "Admin";
+
+
+//-----------Your Password
+$pass_word = "admin12345";
+?>
