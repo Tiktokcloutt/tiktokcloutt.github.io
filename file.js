640f7b006be73720a80aa1c8522909aa2c350312
Tiktokcloutt <154891560+Tiktokcloutt@users.noreply.github.com>
2023-12-30 23:53:06 +0100

    Update file.js

--- file.js
+++ file.js
@@ -6,4 +6,47 @@ const firebaseConfig = {
     storageBucket: "contact-form-5b0d1.appspot.com",
     messagingSenderId: "117353090141",
     appId: "1:117353090141:web:e9ee670b1aa2e852ef51b4"
-  };
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var emailid = getElementVal("emailid");
+  var msgContent = getElementVal("msgContent");
+
+  saveMessages(name, emailid, msgContent);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (name, emailid, msgContent) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    emailid: emailid,
+    msgContent: msgContent,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
