e52348c005bf5b88783d90c9ace244c5111c741b
Tiktokcloutt <154891560+Tiktokcloutt@users.noreply.github.com>
2023-12-30 23:47:51 +0100

    Create file.js

---
+++ file.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAc6ebYdVY7iczL4vtpLFlhC32DfO5O-0",
+    authDomain: "contact-form-5b0d1.firebaseapp.com",
+    databaseURL: "https://contact-form-5b0d1-default-rtdb.firebaseio.com",
+    projectId: "contact-form-5b0d1",
+    storageBucket: "contact-form-5b0d1.appspot.com",
+    messagingSenderId: "117353090141",
+    appId: "1:117353090141:web:e9ee670b1aa2e852ef51b4"
+  };
